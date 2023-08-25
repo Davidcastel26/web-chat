b643a91f82a14108c0d3f6ae8f0c076125196b10
David Castellanos <davcastellanoslarios@gmail.com>
2023-08-25 17:13:06 -0600

    ts running

--- SERVER/src/controller/access.ts
+++ SERVER/src/controller/access.ts
@@ -1,19 +1,20 @@
 // VALIDATE FORM
 const { formSchema } = require("../helpers/common-formSchema")
+import { Request, Response } from 'express'
 // import {formSchema} from '../../common-formSchema'
-import { prismadb} from '../models/prismadb'
+import prismadb from '../models/prismadb'
 
-const access = (req, res) => {
+const access = (req: Request, res: Response) => {
 
     const formData = req.body
 
     formSchema
         .validate(formData)
-        .catch(err => {
+        .catch((err: Error) => {
             res.status(422).send()
-            console.log(err.errors);
+            console.log(err);
         })
-        .then( valid => {
+        .then( (valid:any) => {
             if(valid){
                 res.status(200).send()
                 console.log("form is good in server");
